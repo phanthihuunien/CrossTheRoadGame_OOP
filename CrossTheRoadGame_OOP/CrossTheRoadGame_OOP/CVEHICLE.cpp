d8a9a176cb54d882700ea8de35aebe4d9cca46a7
dtrongnam <88975516+dtrongnam@users.noreply.github.com>
2021-11-23 01:14:12 +0700

    Update Vehicle Class

--- CrossTheRoadGame_OOP/CrossTheRoadGame_OOP/CVEHICLE.cpp
+++ CrossTheRoadGame_OOP/CrossTheRoadGame_OOP/CVEHICLE.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 using namespace std;
 
-CVEHICLE::CVEHICLE(): mX(0), mY(0), prevX(0), prevY(0) {};
-
 void CVEHICLE::Move(int x,int y ){
     mX=x;
     mY=y;
--- CrossTheRoadGame_OOP/CrossTheRoadGame_OOP/CVEHICLE.h
+++ CrossTheRoadGame_OOP/CrossTheRoadGame_OOP/CVEHICLE.h
@@ -1,9 +1,14 @@
 #pragma once
+#include "Header.h"
 class CVEHICLE {
-	int mX, mY;
+protected:
+	int mX, mY,prevX,prevY;
+	int n=3;
+	int m=4;
 public:
 
 	friend class CPEOPLE;
 	virtual void Move(int, int);
-	//…
+	virtual void Draw();
+	//ï¿½
 };
